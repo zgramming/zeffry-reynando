79a039f43730a2a1e6352fc0ca29d6a83b8744e6
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-08-31 21:30:28 +0700

    Move link Font awesome from app.tsx =>document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,13 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"
+          integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
