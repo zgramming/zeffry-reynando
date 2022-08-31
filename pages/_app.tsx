c78bc7ceabdabdab6737b9088a7cb579a00532f2
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-08-31 22:13:16 +0700

    Adding NextNProgress package for loading navigate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import CustomNavbar from "../components/reusable/custom_navbar";
 import CustomFooter from "../components/reusable/custom_footer";
 import Head from "next/head";
+import NextNProgress from "nextjs-progressbar";
 import type { AppProps } from "next/app";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Zeffry Reynando</title>
       </Head>
+      <NextNProgress />
       <CustomNavbar />
       <Component {...pageProps} />
       <CustomFooter />
